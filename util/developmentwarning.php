1369310e73ac9eb5f0470fe9f85350deeec5ca2d
dev_kelz <152704263+devkelz@users.noreply.github.com>
2024-03-04 02:44:54 -0400

    Add files via upload

---
+++ util/developmentwarning.php
@@ -0,0 +1,19 @@
+<!-- development_warning.php -->
+
+<style>
+    .development-warning {
+        background-color: #ffe7d4;
+        color: #97502e;
+        padding: 15px;
+        margin-bottom: 20px;
+        border-radius: 4px;
+    }
+
+    .development-warning strong {
+        color: #97502e;
+    }
+</style>
+
+<div class="development-warning">
+    <strong>Warning!</strong> This website is currently in development.
+</div>
